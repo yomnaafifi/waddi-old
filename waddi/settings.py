9f375e624c02d48fcd042f336a185b5badcbf801
shadyessaam <shadyessam37@gmail.com>
2024-03-11 17:40:00 +0200

    updated .gitignore, apps connected

--- waddi/settings.py
+++ waddi/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'waddi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'waddi',
+        'USER': 'waddi',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',  # or the IP address of your Docker container
+        'PORT': '5432',
     }
 }
 
