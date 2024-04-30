c3175b931cd00562c56dd75527c8a5ddc21bad05
yomnaafifi <yomnaaafifi162@gmail.com>
2024-04-30 04:51:55 +0300

    requested edits

--- customer/models.py
+++ customer/models.py
@@ -9,8 +9,8 @@ from auth.models import CustomUser
 
 class Customer(Address):
     #address = models.
-    customer = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
-    admin = models.ForeignKey(Admin, on_delete = models.CASCADE, null=False)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    #admin = models.ForeignKey(Admin, on_delete = models.CASCADE, null=False)
 
     def __str__(self):
             return self.name
